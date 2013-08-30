deb05d0d255c1978f771a5e8f28bc542c7605278
Frédéric Wang <fred.wang@free.fr>
2013-08-30 12:45:17 +0200

    STIX-Web: move some stretchy operators to fontdata-extra

--- fonts/OpenTypeMath/STIX-Web/config.py
+++ fonts/OpenTypeMath/STIX-Web/config.py
@@ -496,14 +496,35 @@ DELIMITERS = {
 # Delimiters that should be moved to fontdata-extra.js
 DELIMITERS_EXTRA = [
     0x003D, # equal sign
+    0x02C7, # caron
+    0x02CD, # low macron
+    0x02F7, # low tilde
+    0x20D0,
+    0x20D1,
+    0x20D6,
+    0x20E1,
+    0x20EC,
+    0x20ED,
+    0x20EE,
+    0x20EF,
+    0x2140,
     0x219E, # left two-headed arrow
+    0x219F, # upwards two headed arrow
     0x21A0, # right two-headed arrow
+    0x21A1, # downwards two headed arrow
     0x21A4, # left arrow from bar
     0x21A5, # up arrow from bar
     0x21A6, # right arrow from bar
     0x21A7, # down arrow from bar
+    0x21A8, # up down arrow with base
+    0x21A9, # left hook arrow
+    0x21AA, # right hook arrow
     0x21B0, # up arrow with top leftwards
     0x21B1, # up arrow with top right
+    0x21B2, # down arrow with tip left
+    0x21B3, # down arrow with tip right
+    0x21B4, # right arrow with corner down
+    0x21B5, # down arrow with corner left
     0x21BC, # left harpoon with barb up
     0x21BD, # left harpoon with barb down
     0x21BE, # up harpoon with barb right
@@ -512,42 +533,10 @@ DELIMITERS_EXTRA = [
     0x21C1, # right harpoon with barb down
     0x21C2, # down harpoon with barb right
     0x21C3, # down harpoon with barb left
-    0x21DA, # left triple arrow
-    0x21DB, # right triple arrow
-    0x23B4, # top square bracket
-    0x23B5, # bottom square bracket
-    0x23DC, # top paren
-    0x23DD, # bottom paren
-    0x23E0, # top tortoise shell
-    0x23E1, # bottom tortoise shell
-    0x2906, # leftwards double arrow from bar
-    0x2907, # rightwards double arrow from bar
-    0x294E, # left barb up right barb up harpoon
-    0x294F, # up barb right down barb right harpoon
-    0x2950, # left barb dow right barb down harpoon
-    0x2951, # up barb left down barb left harpoon
-    0x295A, # leftwards harpoon with barb up from bar
-    0x295B, # rightwards harpoon with barb up from bar
-    0x295C, # up harpoon with barb right from bar
-    0x295D, # down harpoon with barb right from bar
-    0x295E, # leftwards harpoon with barb down from bar
-    0x295F, # rightwards harpoon with barb down from bar
-    0x2960, # up harpoon with barb left from bar
-    0x2961, # down harpoon with barb left from bar
-    0x02C7, # caron
-    0x02CD, # low macron
-    0x02F7, # low tilde
-    0x219F, # upwards two headed arrow
-    0x21A1, # downwards two headed arrow
-    0x21A8, # up down arrow with base
-    0x21A9, # left hook arrow
-    0x21AA, # right hook arrow
-    0x21B2, # down arrow with tip left
-    0x21B3, # down arrow with tip right
-    0x21B4, # right arrow with corner down
-    0x21B5, # down arrow with corner left
     0x21CB, # left harpoon over right harpoon
     0x21CC, # right harpoon over left harpoon
+    0x21DA, # left triple arrow
+    0x21DB, # right triple arrow
     0x21E0, # left dashed arrow
     0x21E1, # up dashed arrow
     0x21E2, # right dashed arrow
@@ -557,14 +546,51 @@ DELIMITERS_EXTRA = [
     0x21FD, # left open-headed arrow
     0x21FE, # right open-headed arrow
     0x21FF, # left right open-headed arrow
+    0x220F,
+    0x2210,
+    0x2211,
+    0x221B,
+    0x221C,
+    0x222B,
+    0x222C,
+    0x222D,
+    0x222E,
+    0x222F,
+    0x2230,
+    0x2231,
+    0x2232,
+    0x2233,
+    0x22C0,
+    0x22C1,
+    0x22C2,
+    0x22C3,
+    0x23AA,
+    0x23AF,
+    0x23B4, # top square bracket
+    0x23B5, # bottom square bracket
+    0x23D0,
+    0x23DC, # top paren
+    0x23DD, # bottom paren
+    0x23E0, # top tortoise shell
+    0x23E1, # bottom tortoise shell
+    0x2772,
+    0x2773,
     0x27E6, # left white square bracket
     0x27E7, # right white square bracket
     0x27EA, # left double angle bracket
     0x27EB, # right double angle bracket
+    0x27F0,
+    0x27F1,
+    0x2906, # leftwards double arrow from bar
+    0x2907, # rightwards double arrow from bar
     0x290A, # up triple arrow
     0x290B, # down triple arrow
     0x2912, # up arrow to bar
     0x2913, # down arrow to bar
+    0x294E, # left barb up right barb up harpoon
+    0x294F, # up barb right down barb right harpoon
+    0x2950, # left barb dow right barb down harpoon
+    0x2951, # up barb left down barb left harpoon
     0x2952, # left harpoon with barb up to bar
     0x2953, # right harpoon with barb up to bar
     0x2954, # up harpoon with barb right to bar
@@ -573,7 +599,52 @@ DELIMITERS_EXTRA = [
     0x2957, # right harpoon with barb down to bar
     0x2958, # up harpoon with barb left to bar
     0x2959, # down harpoon with barb left to bar
+    0x295A, # leftwards harpoon with barb up from bar
+    0x295B, # rightwards harpoon with barb up from bar
+    0x295C, # up harpoon with barb right from bar
+    0x295D, # down harpoon with barb right from bar
+    0x295E, # leftwards harpoon with barb down from bar
+    0x295F, # rightwards harpoon with barb down from bar
+    0x2960, # up harpoon with barb left from bar
+    0x2961, # down harpoon with barb left from bar
     0x2980, # triple vertical bar
+    0x2983,
+    0x2984,
+    0x2985,
+    0x2986,
     0x2997, # left balck tortoise shell
-    0x2998  # right balck tortoise shell
+    0x2998,  # right balck tortoise shell
+    0x2A00,
+    0x2A01,
+    0x2A02,
+    0x2A03,
+    0x2A04,
+    0x2A05,
+    0x2A06,
+    0x2A07,
+    0x2A08,
+    0x2A09,
+    0x2A0A,
+    0x2A0B,
+    0x2A0C,
+    0x2A0D,
+    0x2A0E,
+    0x2A0F,
+    0x2A10,
+    0x2A11,
+    0x2A12,
+    0x2A13,
+    0x2A14,
+    0x2A15,
+    0x2A16,
+    0x2A17,
+    0x2A18,
+    0x2A19,
+    0x2A1A,
+    0x2A1B,
+    0x2A1C,
+    0x2AFC,
+    0x2AFF,
+    0x2B45,
+    0xFE46
 ]
