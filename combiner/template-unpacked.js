11897ca99eda300d1d468e1008bfc93824d1ad12
Davide P. Cervone <dpvc@union.edu>
2017-04-15 11:34:19 -0400

    Update copyrights

--- combiner/template-unpacked.js
+++ combiner/template-unpacked.js
@@ -2,7 +2,7 @@
  *
  *  %%% NAME %%%
  *  
- *  Copyright (c) 2010-2016 The MathJax Consortium
+ *  Copyright (c) 2010-2017 The MathJax Consortium
  *
  *  Part of the MathJax library.
  *  See http://www.mathjax.org for details.
