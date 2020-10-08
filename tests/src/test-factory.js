49808cd3055d6c0f962c85aacbcf32e1e1affc04
zorkow <volker.sorge@gmail.com>
2020-10-08 17:44:58 +0100

    Adds copyright and file overview.

--- tests/src/test-factory.js
+++ tests/src/test-factory.js
@@ -1,3 +1,27 @@
+/*************************************************************
+ *
+ *  Copyright (c) 2020 The MathJax Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+/**
+ * @fileoverview Factory for tests based on JSON input files.
+ *
+ * @author v.sorge@mathjax.org (Volker Sorge)
+ */
+
 import * as pt from './parser-test.js';
 import {KeyvalTest} from './other-test.js';
 import {JsonTest} from './test.js';
