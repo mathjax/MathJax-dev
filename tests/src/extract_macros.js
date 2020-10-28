37a6dceadc7077cb5769c14e20e792c27c836f76
zorkow <volker.sorge@gmail.com>
2020-10-28 17:58:35 +0000

    Generation of command macro tests.

--- tests/src/extract_macros.js
+++ tests/src/extract_macros.js
@@ -75,21 +75,106 @@ let document = function(packages, output = false) {
 
 
 let convert = function(command, packages) {
-  let root = document(packages).convert(command, {end: STATE.CONVERT});
+  let doc = document(packages);
+  let root = doc.convert(command, {end: STATE.CONVERT});
   root.setTeXclass(null);
-  return jv.visitTree(root);
+  return [jv.visitTree(root), doc.options.InputJax._parseOptions.error];
 };
 
 let collateCharacters = function(map, packages) {
   let result = {};
   for (let [key, char] of map.map) {
-    result[key] = {'input': `\\${key}`, 'expected': convert(`\\${key}`, packages)};
+    let [conversion] = convert(`\\${key}`, packages);
+    result[key] = {'input': `\\${key}`, 'expected': conversion};
   }
   return result;
 };
 
 let collateMacros = function(map) {
   
+};
+
+let specialMacrosMappings = {
+  overwithdelims: 'A\\overwithdelims[ ] B',
+  atopwithdelims: 'A\\atopwithdelims[ ] B',
+  abovewithdelims: 'A\\abovewithdelims[ ] 1pt B',
+  above: 'A\\above 1pt B',
+  root: '\\root 4 \\of x',
+  uproot: '\\sqrt[\\uproot{2}\\beta]{k}',
+  leftroot: '\\sqrt[\\leftroot{-2}\\beta]{k}',
+  left: '\\left(A\\right.',
+  right: '\\left.A\\right)',
+  middle: '\\left.\\middle(A\\right.',
+  rule: '\\rule 2pt 10pt',
+  Rule: '\\Rule{2pt}{10pt}{1cm}',
+  Space: '\\Space{2cm}{2cm}{1cm}',
+  buildrel: '\\buildrel{a}\\over b',
+  skew: '\\skew{5}A B C',
+  mmlToken: '\\mmlToken{mi}{a}',
+  toggle: '\\toggle{A}{B}\\endtoggle',
+  verb: '\\verb+\\A+',
+  unicode: '\\unicode{2525}',
+  genfrac: '\\genfrac{\{}{\}}{10pt}{0}{A}{B}'
+};
+
+
+let specialMacros = function(key, char, packages) {
+  let str = specialMacrosMappings[key] || '';
+  if (key.match(/^(b|B)i(g{1,2})((l|r|m){0,1})$/)) {
+    str = `\\${key}(`;
+  }
+  if (key.match(/^hfi(l+)$/)) {
+    str = `\\begin{array}{c}ab\\\\ \\${key} b\\end{array}`;
+  }
+  if (key.match(/limits$/)) {
+    str = `\\sum\\${key}^A_B`;
+  }
+  if (key.match(/^(raise|lower|move(left|right))$/) ||
+      key.match(/^.{0,1}(skip|space|kern)$/)) {
+    str = `A\\${key} 2pt B C`;
+  }
+  if (key.match(/^shove/)) {
+    str = `\\begin{multline}\\${key} a\\\\ b\\\\ c\\end{multline}`;
+  }
+  if (key.match(/^(cr|h.*line)$/)) {
+    str = `\\array{a\\\\\\${key} b}`;
+  }
+  return [str, str ? convert(str, packages) : null];
+};
+
+let convertMacro = function(key, char, packages) {
+  let [str, conversion] = specialMacros(key, char, packages);
+  if (str && conversion) {
+    return [str, conversion];
+  }
+  let strings = {
+    0: `\\${key} A`,
+    1: `\\${key}{A}{B}`,
+    2: `\\${key}{A}{B}{C}`,
+    3: `\\${key}{A}{B}{C}{D}`
+  };
+  let error = true;
+  let count = 0;
+  while (error && count < 4) {
+    str = strings[count++];
+    [conversion, error] = convert(str, packages);
+  }
+  if (error) {
+    console.log('Error: ' + char._symbol);
+    str = '';
+  }
+  return [str, conversion];
+};
+
+let collateCommands = function(map, packages) {
+  let result = {};
+  for (let [key, char] of map.map) {
+    let [entry, conversion] = convertMacro(key, char, packages);
+    if (entry) {
+      result[key] = {'input': entry, 'expected': conversion};
+    }
+  }
+  return result;
 };
 
 let collateEnvironments = function(map) {
@@ -102,7 +187,7 @@ let writeJson = function(json, name, pkg) {
 };
 
 for (let pkg of ConfigurationHandler.keys()) {
-  console.log(pkg);
+  if (pkg === 'bussproofs' || pkg === 'newcommand') continue;
   let configuration = ConfigurationHandler.get(pkg);
   packageMkdir(configuration);
   let packages = pkg === 'base' ? [pkg] : ['base', pkg];
@@ -122,7 +207,17 @@ for (let pkg of ConfigurationHandler.keys()) {
         writeJson(json, table.name, pkg);
       }
       if (table instanceof sm.EnvironmentMap) continue;
-      if (table instanceof sm.CommandMap) continue;
+      if (table instanceof sm.CommandMap) {
+        console.log(`${pkg}: ${table.name}`);
+        let json = {
+          name: `${pkg} ${table.name} table test`,
+          factory: 'parserTest',
+          packages: packages,
+          tests: collateCommands(table, packages)
+        };
+        writeJson(json, table.name, pkg);
+      }
+      // Not sure what do with those.
       if (table instanceof sm.MacroMap) continue;
     }
   }
