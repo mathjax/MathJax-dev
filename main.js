18139f60a3916efd02b72d053d67f917a437b345
Davide P. Cervone <dpvc@union.edu>
2023-11-14 09:13:37 -0500

    Update lab to include more options available in v4, and update loaders

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-import {mathjax} from './mathjax3/js/mathjax.js';
+import {mathjax} from 'mathjax-full/js/mathjax.js';
 
-import {TeX} from './mathjax3/js/input/tex.js';
-import {RegisterHTMLHandler} from './mathjax3/js/handlers/html.js';
-import {chooseAdaptor} from './mathjax3/js/adaptors/chooseAdaptor.js';
-import {CHTML} from './mathjax3/js/output/chtml.js';
-import {STATE} from './mathjax3/js/core/MathItem.js';
+import {TeX} from 'mathjax-full/js/input/tex.js';
+import {RegisterHTMLHandler} from 'mathjax-full/js/handlers/html.js';
+import {chooseAdaptor} from 'mathjax-full/js/adaptors/chooseAdaptor.js';
+import {CHTML} from 'mathjax-full/js/output/chtml.js';
+import {STATE} from 'mathjax-full/js/core/MathItem.js';
 
 const adaptor = chooseAdaptor();
 RegisterHTMLHandler(adaptor);
 
-const html = mathjax.document('<html></html>', {
+const html = mathjax.document('', {
   InputJax: new TeX(),
-  OutputJax: new CHTML()
+  OutputJax: new CHTML({})
 });
 
 mathjax.handleRetriesFor(() => {
 
-    let math = html.convert(process.argv[3] || '', {end: STATE.TYPESET});
+    let math = html.convert(process.argv[2] || '', {end: STATE.TYPESET});
     console.log(adaptor.outerHTML(math));
 
 }).catch(err => console.log(err.stack));
